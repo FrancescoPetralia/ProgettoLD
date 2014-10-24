5eb4bc724c127a50ec67aaf624d827811a21b0c0
Francesco Petralia <francesco.petralia.89@gmail.com>
2014-10-24 18:22:13 +0200

    file_splitter.py funziona correttamente.

--- ATLD/src/file_splitter.py
+++ ATLD/src/file_splitter.py
@@ -6,35 +6,67 @@ class FileSplitter():
 
     def __init__(self, hosts_number, file_to_split):
 
-        self.hosts_number = hosts_number
+        self.hosts_number = int(hosts_number)
         self.file_to_split = file_to_split
-        self.file_text = None
-        self.total_file_lines = None
+        self.file_content = None
+        self.total_file_lines = 0
         self.lines_per_host = []
+        self.splitted_text_assigned_to_hosts = [[] for x in range(self.hosts_number)]
+        self.splitted_file_name = "splitted_file_"
+        self.lower_index = None
+        self.upper_index = None
 
     def split_file_between_hosts(self):
 
-        # Ricavo il numero di righe del file
-        self.total_file_lines = (self.read_file(self.file_to_split).count("\n") + 1)
+        # Calcolo quante righe ha il file originario
+        for elements in range(0, len(self.read_file(self.file_to_split))):
+            self.total_file_lines = (self.total_file_lines + 1)
 
-        division = (divmod(self.total_file_lines, int(self.hosts_number)))
+        # Calcolo del quoziente e del resto per distribuire il carico di lavoro in modo che sia bilanciato.
+        q, r = (divmod(self.total_file_lines, int(self.hosts_number)))
 
         print("\n")
-        for count in range(0, int(self.hosts_number)):
+        # Al primo host distribuisco le righe in avanzo (resto della divisione)
+        for count in range(0, self.hosts_number):
             if count == 0:
-                self.lines_per_host.append(division[0] + division[1])
-                print("Righe host" + str(count) + ": " + str(self.lines_per_host[count]))
+                self.lines_per_host.append((q + r))
+                print("Righe host_" + str(count) + ": " + str(self.lines_per_host[count]))
             else:
-                self.lines_per_host.append(division[0])
-                print("Righe host" + str(count) + ": " + str(self.lines_per_host[count]))
+                self.lines_per_host.append(q)
+                print("Righe host_" + str(count) + ": " + str(self.lines_per_host[count]))
 
         print("\n")
 
-        return True
+        # Split del file
+        n = -1
+        for count in range(0, self.hosts_number):
+            print("\n")
+            self.lower_index = (n + 1)
+            self.upper_index = self.lower_index + (self.lines_per_host[count])
+            for elements in range(self.lower_index, self.upper_index):
+                self.splitted_text_assigned_to_hosts[count].append(self.file_content[elements])
+                print("elements: " + str(elements))
+                self.write_file(self.splitted_file_name + str(count), self.splitted_text_assigned_to_hosts[count])
+                n = elements
+        tot = n
+        tot = (tot + 1)
+        print(tot)
+
+        # Controllo che lo split del file sia avvenuto correttamente
+        if tot == self.total_file_lines:
+            return True
+        else:
+            return False
 
     def read_file(self, file):
         f = open(file, "r")
-        self.file_text = f.read()
-        #print("\nTesto del file:\n" + self.file_text)
+        self.file_content = f.read().splitlines()
+        #print("\nTesto del file:\n" + str(self.file_content))
         f.close()
-        return self.file_text
+        return self.file_content
+
+    def write_file(self, file_name, interval):
+        f = open("../txt/" + file_name + ".txt", 'w')
+        for elements in interval:
+            f.write(elements + "\n")
+        f.close()
